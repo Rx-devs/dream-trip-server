923dc80df038f5a16a2bdb065bb47bbad22f1603
Tonmoy Roy <rxdevs100@gmail.com>
2022-01-27 19:56:32 +0600

    dreamTrip server init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to DreamTrip!')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at ${port}`);
+});
